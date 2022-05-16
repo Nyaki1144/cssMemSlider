49f6dc8a2fb8a0ddc1d35b2a6864c24d881ec1bb
Nyaki1144 <narek1144@gmail.com>
2022-05-16 11:19:45 +0400

    fix: fix some bug (Mon, May 16, 2022 11:20:32 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -6,17 +6,6 @@ const joke = document.querySelector('.joke-slider');
 
 let num = 512;
 
-
-// point.forEach((el, i) => {
-//     el.addEventListener('click', (e) => {
-//       sliders.style.transform = `translate(-${num*i}px)`
-//       joke.style.transform = `translate(-${num*i}px)`
-//       el.classList.add('activ')
-//     })
-//             el.classList.remove('activ')
-
-// })
-
 point.forEach((el, i) => {
 
     el.addEventListener('click', (e) => {
